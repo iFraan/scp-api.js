e2eb53843e6a5b531a14a94e49284729be525947
Francisco Zorat <franciscozorat@gmail.com>
2024-11-12 14:30:41 -0300

    updated test

--- test.ts
+++ test.ts
@@ -1,20 +1,28 @@
-import { API } from './src';
+import { API, fetchSCP } from './src';
 
 const test = async () => {
     console.log('---- Direct Method: ----')
     try {
-        console.log(await API.fetchSCP('3007'))
+        console.log(await fetchSCP('3007'))
     } catch (e) {
         console.log(e)
     }
+
     console.log('---- Cache Method: ----')
     const api = new API();
     try {
-        await api.getSCP('3007');
-        await api.getSCP('982');
-        console.log(api.scps)
+        const scp1 = await api.getSCP('3007');
+        const scp2 = await api.getSCP('982');
+        console.log('results: ')
+        console.log({
+            scp1,
+            scp2,
+        })
+        console.log('cache: ')
+        console.log(api.raw)
     } catch (e) {
         console.log(e)
     }
 }
+
 test()
